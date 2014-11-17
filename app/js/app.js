371e133817c0b03cd97e68d30d890bee778f8082
Jairo Martinez <jairo@digime.net>
2014-11-17 12:05:31 -0800

    Added Timeout To Loading UI

--- app/js/app.js
+++ app/js/app.js
@@ -5,9 +5,14 @@
 var app = angular.module('app', ['ngResource','ngAnimate','ui.bootstrap', 'd3', 'x2js']);
 
 app.run(function(){
+
 	var gui = require('nw.gui');
-	var tray = new gui.Tray({
-		icon: 'img/red-elephant-16x16.png'
-	});
-	gui.Window.get().show();
+	
+	setTimeout(function(){
+		var tray = new gui.Tray({
+			icon: 'img/red-elephant-16x16.png'
+		});
+		gui.Window.get().show();
+	},30 * 1000);
+
 });
